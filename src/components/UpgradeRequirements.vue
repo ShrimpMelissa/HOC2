03b6d69562933a49532e14ad283d7c008e305246
Shrimpblue <melissashrimpblue@gmail.com>
2024-10-29 16:55:10 -0300

    fix pictures

--- src/components/UpgradeRequirements.vue
+++ src/components/UpgradeRequirements.vue
@@ -2,22 +2,22 @@
 <div id="upgrade-requirements" class="tab-content">
     <h2>Upgrade Requirements</h2>
     <div class="upgrade-buttons">
-        <button onclick="showUpgradeImage('../Images/herostar.png')" style="background-image: url('../Images/Upscaled-stars.png');" data-text="Hero Star"></button>
-        <button onclick="showUpgradeImage('../Images/potential.png')" style="background-image: url('../Images/Upscaled-potential.png');" data-text="Potential"></button>
-        <button onclick="showUpgradeImage('../Images/transfer.png')" style="background-image: url('../Images/Upscaled-transfer.png');" data-text="Transfer"></button>
-        <button onclick="showUpgradeImage('../Images/upanishad.png')" style="background-image: url('../Images/Upscaled-upanishad.png');" data-text="Upanishad"></button>
-        <button onclick="showUpgradeImage('../Images/rebirth.png')" style="background-image: url('../Images/Upscaled-rebirth.png');" data-text="Rebirth"></button>
-        <button onclick="showUpgradeImage('../Images/divinity.png')" style="background-image: url('../Images/Upscaled-crown.png');" data-text="Divinity"></button>
-        <button onclick="showUpgradeImage('../Images/runestone.png')" style="background-image: url('../Images/Upscaled-runestone.png');" data-text="Runestone"></button>
-        <button onclick="showUpgradeImage('../Images/talisman.png')" style="background-image: url('../Images/Upscaled-origin.png');" data-text="Talisman"></button>
-        <button onclick="showUpgradeImage('../Images/spirits.png')" style="background-image: url('../Images/Upscaled-spirit.png');" data-text="Spirits"></button>
-        <button onclick="showUpgradeImage('../Images/starhubgear.png')" style="background-image: url('../Images/Upscaled-starhub.png');" data-text="Star Hub Gear"></button>
-        <button onclick="showUpgradeImage('../Images/treasure.png')" style="background-image: url('../Images/Upscaled-treasure.png');" data-text="Treasure"></button>
-        <button onclick="showUpgradeImage('../Images/pet.png')" style="background-image: url('../Images/Upscaled-pet.png');" data-text="Pet"></button>
-        <button onclick="showUpgradeImage('../Images/codex.png')" style="background-image: url('../Images/Upscaled-codex.png');" data-text="Codex"></button>
-        <button onclick="showUpgradeImage('../Images/energysource.png')" style="background-image: url('../Images/Upscaled-source.png');" data-text="Energy Source"></button>
-        <button onclick="showUpgradeImage('../Images/artifact.png')" style="background-image: url('../Images/Upscaled-artifact.png');" data-text="Artifact"></button>
-        <button onclick="showUpgradeImage('../Images/goddessskin.png')" style="background-image: url('../Images/Upscaled-goddess.png');" data-text="Goddess Skin"></button>
+        <button @click="showUpgradeImage('herostar.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-stars.png')})` }" data-text="Hero Star"></button>
+        <button @click="showUpgradeImage('potential.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-potential.png')})` }" data-text="Potential"></button>
+        <button @click="showUpgradeImage('transfer.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-transfer.png')})` }" data-text="Transfer"></button>
+        <button @click="showUpgradeImage('upanishad.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-upanishad.png')})` }" data-text="Upanishad"></button>
+        <button @click="showUpgradeImage('rebirth.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-rebirth.png')})` }" data-text="Rebirth"></button>
+        <button @click="showUpgradeImage('divinity.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-crown.png')})` }" data-text="Divinity"></button>
+        <button @click="showUpgradeImage('runestone.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-runestone.png')})` }" data-text="Runestone"></button>
+        <button @click="showUpgradeImage('talisman.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-origin.png')})` }" data-text="Talisman"></button>
+        <button @click="showUpgradeImage('spirits.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-spirit.png')})` }" data-text="Spirits"></button>
+        <button @click="showUpgradeImage('starhubgear.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-starhub.png')})` }" data-text="Star Hub Gear"></button>
+        <button @click="showUpgradeImage('treasure.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-treasure.png')})` }" data-text="Treasure"></button>
+        <button @click="showUpgradeImage('pet.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-pet.png')})` }" data-text="Pet"></button>
+        <button @click="showUpgradeImage('codex.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-codex.png')})` }" data-text="Codex"></button>
+        <button @click="showUpgradeImage('energysource.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-source.png')})` }" data-text="Energy Source"></button>
+        <button @click="showUpgradeImage('artifact.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-artifact.png')})` }" data-text="Artifact"></button>
+        <button @click="showUpgradeImage('goddessskin.png')" :style="{ backgroundImage: `url(${getImageUrl('Upscaled-goddess.png')})` }" data-text="Goddess Skin"></button>
     </div>
 </div>
 
@@ -26,13 +26,24 @@
 <script>
 export default {
   name: 'UpgradeRequirements',
+  methods: {
+    getImageUrl(imageName) {
+      return `/HOC2/src/Images/${imageName}`
+    },
+    showUpgradeImage(imageName) {
+      // Add your image showing logic here
+      console.log(`Showing image: ${this.getImageUrl(imageName)}`)
+    }
+  }
 }
 </script>
 
 <style scoped>
-            .upgrade-buttons button {
-                width: 80px;
-                height: 80px;
-                margin: 5px;
-            }
+.upgrade-buttons button {
+    width: 80px;
+    height: 80px;
+    margin: 5px;
+    background-size: cover;
+    background-position: center;
+}
 </style>
